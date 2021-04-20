5853b92e61add924b6d4023168a2f38ddec21e89
Ubuntu <shrivardhanlimbkar@shri.fx1ni0uhg5ve1fh1v0czt1r2pa.ax.internal.cloudapp.net>
2021-04-20 14:06:02 +0000

    initial setup

--- api/app.py
+++ api/app.py
@@ -1,52 +1,18 @@
 from flask import Flask
 from flask import request
-from flask_mysqldb import MySQL
-from flask_cors import CORS
-import json
-mysql = MySQL()
+
 app = Flask(__name__)
-CORS(app)
-# My SQL Instance configurations
-# Change the HOST IP and Password to match your instance configurations
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = ''
-app.config['MYSQL_DB'] = 'student'
-app.config['MYSQL_HOST'] = 'localhost'  # for now
-mysql.init_app(app)
 
 
-@app.route("/add")  # Add Student
-def add():
-    name = request.args.get('name')
-    email = request.args.get('email')
-    cur = mysql.connection.cursor()  # create a connection to the SQL instance
-    s = '''INSERT INTO students(studentName, email) VALUES('{}','{}');'''.format(
-        name, email)
-    cur.execute(s)
-    mysql.connection.commit()
-
-    return '{"Result":"Success"}'
-
-
-@app.route("/")  # Default - Show Data
+@app.route("/api/")  # URL leading to method
 def hello():  # Name of the method
-    cur = mysql.connection.cursor()  # create a connection to the SQL instance
-    cur.execute('''SELECT * FROM students''')  # execute an SQL statment
-    rv = cur.fetchall()  # Retreive all rows returend by the SQL statment
-    Results = []
-    for row in rv:  # Format the Output Results and add to return string
-        Result = {}
-        Result['Name'] = row[0].replace('\n', ' ')
-        Result['Email'] = row[1]
-        Result['ID'] = row[2]
-        Results.append(Result)
-    response = {'Results': Results, 'count': len(Results)}
-    ret = app.response_class(
-        response=json.dumps(response),
-        status=200,
-        mimetype='application/json'
-    )
-    return ret  # Return the data in a string format
+    return("Welcome to Rating App API page.")
+
+
+@app.route("/greetme")  # different URL
+def helloall():  # different method name
+    name = request.args.get('name')  # retrieve GET parameters
+    return("Hello Shri {}!".format(name))  # Python’s string.format
 
 
 if __name__ == "__main__":
