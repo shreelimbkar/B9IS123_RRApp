561867e4376a2675235f8d3d816ef4e333f54278
Bijendra-DBS <bijendrasigh9146@gmail.com>
2021-04-30 12:04:30 +0100

    some chnages

--- front-end/src/app/shared/rest-url-constant.ts
+++ front-end/src/app/shared/rest-url-constant.ts
@@ -12,19 +12,4 @@ export class RestUrl {
   public static get sendOTP(): string { return `users/checkotp`; }
   public static get writeReview(): string { return `reviews/add`; }
   public static get getUserReview(): string { return `reviews?category=`; }
-
-
-
-
-
-  public static get getDetail(): string { return `/`; }
-  public static get mostViewedProperty(): string { return `/mostViewedProperty`; }
-  public static get home(): string { return `/houseForRent`; }
-  public static get listingProperty(): string { return `/ListOfProperty`; }
-  public static get ascendingSort(): string { return `?_sort=Price&_order=asc`; }
-  public static get descendingSort(): string { return `?_sort=Price&_order=desc`; }
-
-
-
-
 }
